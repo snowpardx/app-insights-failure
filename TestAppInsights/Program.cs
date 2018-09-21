87c31ef46c3a69be5b980d411822cb0d482e8af9
Alex Kharchenko <aleksey.kharchenko@akvelon.com>
2018-09-21 08:20:36 -0700

    Reproduced error

--- TestAppInsights/Program.cs
+++ TestAppInsights/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.AspNetCore.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace TestAppInsights
 {
@@ -10,6 +12,22 @@ namespace TestAppInsights
     {
         static void Main(string[] args)
         {
+            var loggerFactory = new LoggerFactory();
+
+            var serviceCollection = new ServiceCollection()
+                .AddSingleton<ILoggerFactory>(loggerFactory)
+                .AddSingleton<IHostingEnvironment>(new HostingEnvironment() { ContentRootPath = Directory.GetCurrentDirectory() })
+                .AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+                {
+                   DeveloperMode = true,
+                   InstrumentationKey = "your instrumentation key",
+                })
+                .Configure<ApplicationInsightsLoggerOptions>(o => o.IncludeEventId = true);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            loggerFactory.AddApplicationInsights(serviceProvider, LogLevel.Trace);
+
+            var logger = loggerFactory.CreateLogger("Sample logger");
+            logger.LogWarning("Sample {data} logged", 12345);
         }
     }
 }
